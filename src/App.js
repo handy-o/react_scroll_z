e0e20d1e8c696d97f0aeb10f095bdf87f116dad8
handy-o <dyhan9420@gmail.com>
2022-07-30 11:05:20 +0900

    프로젝트 구성, css 초기화

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import GlobalStyles from './GlobalStyles';
+
+function App() {
+	return (
+		<div>
+			<GlobalStyles />
+			<h1>Hello</h1>
+			<ul>
+				<li>list1</li>
+				<li>list2</li>
+				<li>list3</li>
+			</ul>
+		</div>
+	);
+}
+
+export default App;
